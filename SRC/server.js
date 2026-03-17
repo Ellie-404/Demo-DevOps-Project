ecdb934df7d2ea68b6f33da60a7c89b027067107
Elise <Ebjerkelo@outlook.com>
2026-03-17 11:12:27 +0100

    .

--- SRC/server.js
+++ SRC/server.js
@@ -27,5 +27,15 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
+app.get("/start", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "homePage.html"));
+});
+app.get("/weather", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "weatherPage.html"));
+});
+app.get("/user", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "userPage.html"));
+});
+
 //Server and what port it should listen to
 app.listen(3000, () => console.log("Server runs on http://localhost:3000"));
